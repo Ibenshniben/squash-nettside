a421f69176b85bace7c906a8fd983f84e6d7233b
Ibenshniben <151071452+Ibenshniben@users.noreply.github.com>
2025-03-19 10:39:46 +0100

    fix it all 12312313

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,5 @@
+// Move the authOptions to this file
+import { authOptions } from './auth'
+
+// Export it from here
+export { authOptions }
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,5 @@ import { authOptions } from './auth'
 // Create the handler using the imported authOptions
 const handler = NextAuth(authOptions)
 
-// Re-export authOptions so other files can import it from this file
-export { authOptions }
-
-// Export the handler functions
+// Export only the handler functions
 export { handler as GET, handler as POST }
