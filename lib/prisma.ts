47db74ea6a34ed56d4a0c41bdd8a5106835d6de7
Ibenshniben <151071452+Ibenshniben@users.noreply.github.com>
2025-03-24 10:04:29 +0100

    uyguyv

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const prisma = new PrismaClient();
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-// Remove this incorrect import - it's creating a circular dependency
-// import { auth } from '@/lib/auth.ts'
+export { prisma };
